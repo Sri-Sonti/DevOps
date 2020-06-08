4d346af216e3c153d35526ff2a17aff83f30075a
Sri-Sonti <36663964+Sri-Sonti@users.noreply.github.com>
2020-06-08 18:17:46 +0100

    Update main.tf

--- Terraform/Guacamole/mod_docker/main.tf
+++ Terraform/Guacamole/mod_docker/main.tf
@@ -6,7 +6,7 @@ variable "name" {
 # Configure the Docker provider
 provider "docker" {
   host = "tcp://192.168.99.102:2376"
-  cert_path="/mnt/c/Users/Srikanth/.docker/machine/machines/default"
+  cert_path="/mnt/c/Users/Sri/.docker/machine/machines/default"
 }
 
 
